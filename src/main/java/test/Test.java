49b7a864ed9dc58e1f4bdc8600615b01dca818c6
atiq <joseph1atiq@gmail.com>
2017-08-23 23:44:37 +0200

    Creation of employees

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -2,6 +2,7 @@ package test;
 
 
 import org.gestion.banque.entities.Client;
+import org.gestion.banque.entities.Employe;
 import org.gestion.banque.metier.IBanqueMetier;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -14,6 +15,11 @@ public class Test {
 		
 		metier.addClient(new Client("C1", "AD1"));
 		metier.addClient(new Client("C2", "AD2"));
+		metier.addEmploye(new Employe("E1"), null);
+		metier.addEmploye(new Employe("E2"), 1L);
+		metier.addEmploye(new Employe("E3"), 1L);
+
+		
 
 		
 		     
